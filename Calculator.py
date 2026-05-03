55c84d2d6459ec53861b42bfb4c57a751c639db0
Shruti Sinha <22f3003063@ds.study.iitm.ac.in>
2026-05-04 00:45:26 +0530

    Initial Commit

---
+++ Calculator.py
@@ -0,0 +1,46 @@
+def add(a,b):
+    return (a+b)
+
+def substract(a,b):
+    return(a-b)
+
+def multiply(a,b):
+    return(a*b)
+
+def divide(a,b):
+    try:
+        return (a/b)
+    except ZeroDivisionError:
+        return "Error: Cannot divide by zero"
+
+while True:
+    print("Select Operation")
+    print("1. Add")
+    print("2. Substract")
+    print("3. Multiply")
+    print("4. Divide")
+    print("5. Exit")
+
+    operation = input("Enter the operation(1/2/3/4/5) ")
+
+    if operation == '5':
+        break
+
+    try:
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+    except ValueError:
+        print("Invalid Input: Please enter a number")
+        pass
+
+    if operation == '1':
+        print("Result: ", add(num1,num2))
+
+    if operation == '2':
+        print("Result: ", substract(num1,num2))
+
+    if operation == '3':
+        print("Result: ", multiply(num1,num2))
+
+    if operation == '4':
+        print("Result: ", divide(num1,num2))
